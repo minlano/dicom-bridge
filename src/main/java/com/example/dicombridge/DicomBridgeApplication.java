23b6048e241f073b704887e26e562c6f23a61ff6
jonghechoi <sosinnmi@naver.com>
2023-11-01 14:57:43 +0900

    Initial Commit

---
+++ src/main/java/com/example/dicombridge/DicomBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dicombridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicomBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DicomBridgeApplication.class, args);
+    }
+
+}
