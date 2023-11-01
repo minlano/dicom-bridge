21a962b5d73fce30bf320e716ffe8e582737feb2
jeonghoonoh <jhoh416@gmail.com>
2023-11-01 17:33:55 +0900

    test: 이미지 렌더링 확인
    500 에러 발생. 수정 필요
    
    오정훈

---
+++ src/main/java/com/example/dicombridge/controller/DicomImageController.java
@@ -0,0 +1,44 @@
+package com.example.dicombridge.controller;
+
+import org.dcm4che3.data.Attributes;
+
+import org.dcm4che3.imageio.plugins.dcm.DicomImageReader;
+import org.dcm4che3.imageio.plugins.dcm.DicomImageReaderSpi;
+
+import org.dcm4che3.io.DicomInputStream;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+@Controller
+public class DicomImageController {
+
+
+    @GetMapping("/viewDicomImage")
+    public String viewDicomImage(Model model) {
+        try {
+            String filePath = "/Users/jeonghoonoh/Downloads/DCM-Sample4KDT/CR-Chest PA/1.2.410.200013.1.510.1.20210310170346701.0009.dcm";
+            File dicomFile = new File(filePath);
+
+            DicomInputStream dis = new DicomInputStream(dicomFile);
+            Attributes attrs = dis.readDataset(-1, -1);
+
+            DicomImageReaderSpi spi = new DicomImageReaderSpi();
+            ImageInputStream iis = ImageIO.createImageInputStream(dicomFile);
+            ImageReader reader = spi.createReaderInstance();
+            reader.setInput(iis, false);
+            BufferedImage image = reader.read(0);
+
+            model.addAttribute("dicomImage", image);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "dicomImagePage";
+    }
+}
