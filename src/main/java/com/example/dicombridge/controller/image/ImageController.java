85de9eddb925968f671db08d1bc9b7dbe9277700
sunsohyun <shisun9952@gmail.com>
2023-11-03 17:25:21 +0900

    feat: Dcm4che 이미지 렌더링
    선소현

--- src/main/java/com/example/dicombridge/controller/image/ImageController.java
+++ src/main/java/com/example/dicombridge/controller/image/ImageController.java
@@ -1,7 +1,8 @@
 package com.example.dicombridge.controller.image;
 
-import com.example.dicombridge.service.image.ImageService;
+
 import org.dcm4che3.imageio.plugins.dcm.DicomImageReadParam;
+import org.dcm4che3.io.DicomInputStream;
 import org.dcm4che3.tool.dcm2jpg.Dcm2Jpg;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,16 +14,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.imageio.ImageIO;
-import javax.imageio.ImageReadParam;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.ImageInputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Base64;
-import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.jar.Attributes;
+
 
 @Controller
 public class ImageController {
@@ -31,7 +31,7 @@ public class ImageController {
     @GetMapping("/showDicomImage")
     public String showDicomImage(HttpServletResponse response) {
         // 이미지 파일 경로
-        String dicomImagePath = "C:/Users/TJ/Documents/선소현/DCM-Sample4KDT/CT-Abdomen/1.3.12.2.1107.5.1.4.65266.30000018122721584475300010337.dcm";
+        String dicomImagePath = "C:/Users/TJ/Documents/선소현/DCM-Sample4KDT/CR-Chest PA/1.2.410.200013.1.510.1.20210310170346701.0009.dcm";
 
         try {
             File dicomFile = new File(dicomImagePath);
@@ -42,6 +42,7 @@ public class ImageController {
 
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             ImageIO.write(image, "jpeg", baos);
+
 
             //이미지데이터를 바이트 배열로 복사해 imageBytes 변수에 저장
             byte[] imageData  = baos.toByteArray();
@@ -62,22 +63,68 @@ public class ImageController {
     }
 
 
-//    @GetMapping("/showDicomImage")
-//    public ResponseEntity<byte[]> showDicomImage(@RequestParam("referencedSOPInstanceUID") String referencedSOPInstanceUID) {
-//        try {
-//            byte[] imageData = ImageService.getUID(referencedSOPInstanceUID);
+//    @GetMapping("/DicomImage")
+//    public void DicomImage(HttpServletResponse response) {
+//        // DICOM 파일 경로
+//        String dicomPath = "C:/Users/TJ/Documents/선소현/DCM-Sample4KDT/CR-Chest PA";
 //
-//            if (imageData != null) {
-//                HttpHeaders headers = new HttpHeaders();
-//                headers.setContentType(MediaType.IMAGE_JPEG);
-//                return new ResponseEntity<>(imageData, headers, HttpStatus.OK);
-//            } else {
-//                return new ResponseEntity<>(HttpStatus.NOT_FOUND); // 이미지를 찾지 못한 경우
-//            }
+//        try {
+//            Dcm2Jpg dcm2Jpg = new Dcm2Jpg();
+//            dcm2Jpg.initImageWriter("JPEG", "jpg", "com.sun.imageio.plugins.jpeg.JPEGImageWriter", null, null);
+//
+//            File dicomFile = new File(dicomPath);
+//
+//            // DICOM 파일을 이미지로 변환
+//            BufferedImage image = dcm2Jpg.readImageFromDicomInputStream(dicomFile);
+//
+//            // 이미지 전송
+//            response.setContentType("image/jpeg");
+//            OutputStream out = response.getOutputStream();
+//
+//            // 이미지를 JPEG 형식으로 출력 스트림에 작성
+//            ImageIO.write(image, "jpeg", out);
+//            out.flush();
+//
 //        } catch (IOException e) {
-//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); // 오류 발생시
+//            e.printStackTrace();
 //        }
 //    }
+
+
+    @GetMapping("/showDicomImages")
+    public List<String> showDicomImages(@RequestParam String path) {
+        List<String> imageUrls = new ArrayList<>();
+
+        File dicomFiles = new File(path);
+
+        if (dicomFiles.isDirectory()) {
+            File[] files = dicomFiles.listFiles((dir, name) -> name.toLowerCase().endsWith(".dcm"));
+
+            if (files != null) {
+                for (File file : files) {
+                    try {
+                        Dcm2Jpg dcm2Jpg = new Dcm2Jpg();
+                        BufferedImage image = dcm2Jpg.readImageFromDicomInputStream(file);
+
+                        if (image != null) {
+                            String imageUrl = "images/" + file.getName() + ".jpg"; // 이미지 파일 경로
+                            ImageIO.write(image, "jpg", new File(imageUrl));
+                            imageUrls.add(imageUrl);
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        return imageUrls;
+
+    }
+
+
+
 
 
 
--- src/main/webapp/WEB-INF/views/showImage.jsp
+++ src/main/webapp/WEB-INF/views/showImage.jsp
@@ -5,6 +5,38 @@
 </head>
 <body>
     <h1>DICOM Image</h1>
-    <img src="data:image/jpeg;base64,${dicomImage}" alt="DICOM Image">
+<%--    <img src="data:image/jpeg;base64,${dicomImage}" alt="DICOM Image">--%>
+
+<%--    <img src="/showDicomImage" alt="DICOM Image">--%>
+    <div>
+        <a href="#" onclick="showDicomImages('Z:/201608/22/MS0010/MR/7')">Convert DCM to Images</a>
+    </div>
+    <div id="imagesContainer">
+    </div>
+    <script>
+        function showDicomImages(path) {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/showDicomImages?path=" + path, true);
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    var data = JSON.parse(xhr.responseText);
+                    if (data.length > 0) {
+                        var imagesContainer = document.getElementById("imagesContainer");
+                        imagesContainer.innerHTML = ""; // 이미지 컨테이너 초기화
+                        for (var i = 0; i < data.length; i++) {
+                            var imageUrl = data[i];
+                            var img = document.createElement("img");
+                            img.src = imageUrl;
+                            imagesContainer.appendChild(img);
+                        }
+                    } else {
+                        alert("No images found.");
+                    }
+                }
+            };
+            xhr.send();
+        }
+    </script>
+
 </body>
 </html>
