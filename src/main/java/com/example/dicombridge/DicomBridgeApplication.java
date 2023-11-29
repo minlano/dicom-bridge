21c04a7cb97dbb77d1c4109934cf788d4b56584d
jonghechoi <sosinnmi@naver.com>
2023-11-29 11:28:40 +0900

    feat: cors 매핑 추가
    최종희

--- src/main/java/com/example/dicombridge/DicomBridgeApplication.java
+++ src/main/java/com/example/dicombridge/DicomBridgeApplication.java
@@ -3,6 +3,9 @@ package com.example.dicombridge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableCaching
@@ -11,4 +14,17 @@ public class DicomBridgeApplication {
         SpringApplication.run(DicomBridgeApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:8080", "http://192.168.30.93:8080");
+                registry.addMapping("API_PATH/**").allowedOrigins("http://localhost:8080", "http://192.168.30.93:8080")
+                        .allowedMethods("GET", "POST")
+                        .allowedHeaders("Origin", "Content-Type", "Accept")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
